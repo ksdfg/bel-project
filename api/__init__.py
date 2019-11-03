2a126fd6a4969c545ba668a760420d39778b5a7c
ksdfg <ksdfg123@gmail.com>
2019-11-03 13:16:39 +0530

    Initial commit

---
+++ api/__init__.py
@@ -0,0 +1,102 @@
+from json import dumps
+
+from flask import Flask, request
+from mysql.connector import connect, IntegrityError
+
+# create the flask app
+app = Flask('bel crm')
+
+# database object, connects to mysql
+db = connect(
+    host="localhost",
+    user="belmaster",
+    passwd="master",
+    database="beldb"
+)
+dbcursor = db.cursor()  # cursor that'll allow us to execute queries
+
+
+# post request to login
+@app.route('/api/login', methods=["POST"])
+def validate_login():
+    try:
+        dbcursor.execute(
+            "Select password, role from user where username = '{}'".format(request.form['username']))  # get password
+        res = dbcursor.fetchone()
+        if request.form['password'] in res:
+            return dumps({'result': 'true', 'role': res[1]})
+        else:
+            return 'false'
+    except Exception as e:
+        print(e)
+        return str(e)
+
+
+# post request to register a new user
+@app.route('/api/register', methods=["POST"])
+def register():
+    # first register user
+    try:
+        dbcursor.execute("Insert into user values (%s, %s, %s)", (request.form['username'], request.form['password'],
+                                                                  request.form['role']))
+    except IntegrityError as e:
+        print(e)
+        return 'Username already taken'
+    except Exception as e:
+        print('user', e.__class__)
+        return str(e)
+
+    # now add the overall entity
+    try:
+        # add the respective entity
+        if request.form['role'] == 'engineer':
+            dbcursor.execute("Insert into engineer(Name, ContactNo, email, Region, Address, Username) values "
+                             "(%s, %s, %s, %s, %s, %s)",
+                             (
+                                 request.form['name'], int(request.form['contact']), request.form['email'],
+                                 request.form['region'], request.form['address'], request.form['username']
+                             ))
+        elif request.form['role'] == 'reg_mgr':
+            dbcursor.execute("Insert into reg_center values (%s, %s)", (request.form['id'], request.form['address']))
+        elif request.form['role'] == 'bel_mgr':
+            pass
+        elif request.form['role'] == 'call_centre':
+            pass
+        else:
+            return "What role is this?"
+
+        db.commit()  # change ought to be permanent
+        return 'done'
+    except Exception as e:
+        print('entity', request.form['role'], e.__class__)
+        return str(e)
+
+
+@app.route('/api/retrieve/homepage-data', methods=['GET'])
+def homepage_date():
+    print(request.form['role'])
+    data = dict()
+
+    if request.form['role'] == 'bel_mgr':
+
+        # get total scrap value
+        dbcursor.execute(
+            "Select round(sum(m.price * e.Qty), 3) from eng_scrap e join material m on e.PartNo = m.PartNo")
+        res = dbcursor.fetchone()
+        data['scrap_money'] = res[0]
+        dbcursor.execute(
+            "Select round(sum(m.price * r.Qty), 3) from reg_scrap r join material m on r.PartNo = m.PartNo")
+        res = dbcursor.fetchone()
+        data['scrap_money'] += res[0]
+
+        # get high priority complaints
+        dbcursor.execute(
+            "Select t.Machine, c.Name, m.Location, e.Name "
+            "from (complaint t join engineer e on t.Engineer = e.ID) join "
+            "(machine m join customer c on c.ID = m.CustID) on t.Machine = m.SlNo "
+            "where priority = 'High'"
+        )
+        res = dbcursor.fetchall()
+        data['complaints'] = res
+
+    return dumps(data)
---
+++ web_app/__init__.py
@@ -0,0 +1,56 @@
+from json import loads
+
+from flask import render_template, redirect, url_for, request, session
+from requests import get, post
+
+from api import app
+
+app.template_folder = r'K:\bel-project\web_app\templates'
+app.secret_key = 'IB6TBIUKYBGF76VD'
+
+url = "http://localhost:5000/"
+
+
+@app.route('/')
+def homepage():
+    if 'username' not in session:
+        return redirect(url_for('login_page'), 307)
+    data = loads(get(url=url+'api/retrieve/homepage-data', data=dict(session)).text)
+    return render_template(
+        'homepage.html',
+        username=session['username'],
+        **data
+    )
+
+
+@app.route('/login')
+def login_page():
+    if 'username' in session:
+        return render_template('login.html', username=session['username'])
+    return render_template('login.html')
+
+
+@app.route('/login/submit', methods=['POST'])
+def login_submit():
+    response = loads(post(url=url + "api/login", data=dict(request.form)).text)
+    if response['result'] == 'true':
+        session['username'] = request.form['username']
+        session['role'] = response['role']
+        return redirect(url_for('homepage'))
+    return render_template('login.html', error="Incorrect Password", username=request.form['name'])
+
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html', role=request.args.get('role'))
+
+
+@app.route('/register/submit', methods=["POST"])
+def register_submit():
+    if request.form['password'] != request.form['confirm password']:
+        return render_template("register.html", **request.form, error="passwords don't match dummo, try again")
+    res = post(url + "api/register", data=dict(request.form)).text
+    if res == 'done':
+        return redirect(url_for('login_page'))
+    else:
+        return render_template('register.html', **request.form, error=res)
